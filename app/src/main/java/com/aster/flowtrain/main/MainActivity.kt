3c28433f36176e90f3e496e5f3d5d330b9ba6ac9
Achmad Ichsan <achmad.ichsan@dana.id>
2022-04-09 16:16:55 +0700

    optimize imports on MainActivity.kt

--- app/src/main/java/com/aster/flowtrain/main/MainActivity.kt
+++ app/src/main/java/com/aster/flowtrain/main/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.aster.flowtrain.main
 
-import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.aster.domain.base.Result
 import com.aster.flowtrain.base.activity.BaseActivity
 import com.aster.flowtrain.databinding.ActivityMainBinding
-import com.aster.flowtrain.home.adapter.ArticlesAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {

3d07ba6c44617279c92e222fb39b8a114e85d2a6
Achmad Ichsan <achmad.ichsan@dana.id>
2022-04-11 15:42:46 +0700

    add base dialog

--- app/src/main/java/com/aster/flowtrain/base/dialog/BaseDialogFragment.kt
+++ app/src/main/java/com/aster/flowtrain/base/dialog/BaseDialogFragment.kt
@@ -1,10 +1,31 @@
 package com.aster.flowtrain.base.dialog
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
 
 /**
  * @author ichsanachmad
  */
-abstract class BaseDialogFragment : DialogFragment() {
+abstract class BaseDialogFragment<Binding: ViewBinding> : DialogFragment() {
+    open val binding: Binding by lazy { getViewBinding() }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    abstract fun init()
+    abstract fun getViewBinding(): Binding
 }
